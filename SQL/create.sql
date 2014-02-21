0e5291f5ef058407cfeef17d1b9c6b71dc1fb21b
James <euesdenje@gmail.com>
2014-02-21 14:30:07 +0000

    Updated Docs, reformat some SQL, update Sshot
    Updated the documentation to conform with the UID/name changes.
    Reformatted some of the SQL to look a little nice on paper in the
    documentation.
    Updated the screenshots to display the table creation.
    Begun work on explaining my choices of data types.

--- SQL/create.sql
+++ SQL/create.sql
@@ -17,12 +17,15 @@
 
 -- Create a sequence for incrementing the piste_uid
 CREATE SEQUENCE piste_uid_seq;
--- Create a type for grading and open for Piste
+
+-- Create a type for grading for Piste
 CREATE TYPE grade_rank as ENUM('EASY', 'MEDIUM', 'HARD', 'DIFFICULT');
 
--- Create the Piste table. PK UID. Unique Names.
+-- Create the Piste table.
+-- PK UID. Unique Names.
 CREATE TABLE piste (
-	piste_uid int DEFAULT nextval('piste_uid_seq') NOT NULL PRIMARY KEY,
+	piste_uid int DEFAULT nextval('piste_uid_seq')
+		NOT NULL PRIMARY KEY,
 	piste_name varchar(50) UNIQUE NOT NULL,
 	grade grade_rank NOT NULL DEFAULT 'EASY',
 	length_km real NOT NULL,
@@ -33,12 +36,15 @@ ALTER SEQUENCE piste_uid_seq OWNED BY piste.piste_uid;
 	
 -- Create a sequence for incrementing the lift_uid	
 CREATE SEQUENCE lift_uid_seq;
+
 -- Create a type for the type and operating Lifts
 CREATE TYPE type_lift as ENUM('GONDOLA', 'CHAIR', 'TOW');
 	
--- Create the Lift table. PK UID. Unique Names.
+-- Create the Lift table.
+-- PK UID. Unique Names.
 CREATE TABLE lift (
-	lift_uid int DEFAULT nextval('lift_uid_seq') NOT NULL PRIMARY KEY,
+	lift_uid int DEFAULT nextval('lift_uid_seq')
+		NOT NULL PRIMARY KEY,
 	lift_name varchar(60) UNIQUE NOT NULL,
 	lift_type type_lift NOT NULL,
 	summit_m integer NOT NULL,
@@ -48,7 +54,8 @@ CREATE TABLE lift (
 );
 ALTER SEQUENCE lift_uid_seq OWNED BY lift.lift_uid;
 
--- Create the table Connections (M-M). PK is UIDs, Names for content.
+-- Create the table Connections (M-M).
+-- PK is UIDs, Names for content.
 CREATE TABLE connections (
 	piste_uid integer NOT NULL,
 	piste_name varchar(50) NOT NULL,
